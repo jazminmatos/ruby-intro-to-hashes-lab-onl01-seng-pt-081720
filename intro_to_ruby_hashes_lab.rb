d6dc7effa6c9cdcfe35a1737c79ea92910535c5c
Jazmin Matos <jmatos1993@gmail.com>
2020-08-27 18:42:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,11 @@ def actor #create and return hash w/ certain actor's name
 end
 
 def monopoly
-	monopoly = {railroads: {pieces: 4}}
+	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {railroads: {pieces: 4}}
 
 end
 
